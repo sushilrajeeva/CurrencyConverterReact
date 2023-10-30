e92f5ce0658b113404f679f873d928de3e761a26
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2023-10-29 23:04:00 -0400

    Created useCurrencyInfo Custom Hook
    This hook fetches currency from the url -> https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json and shows a json of conversion of that currency to other currencies

---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+
+function useCurrencyInfo(currency) {
+  //storing the fetch data
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch(
+      //This url has the currency json of a currency to all other currency (latest)
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+
+    console.log(data);
+  }, [currency]);
+
+  return data;
+}
+
+export default useCurrencyInfo;
