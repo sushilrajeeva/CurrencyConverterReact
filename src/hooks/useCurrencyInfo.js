99feb5df5e19504dab9d8e854d78b0c9054a250b
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2024-11-18 04:42:15 -0500

    BugFix: Currency API migration

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -7,7 +7,8 @@ function useCurrencyInfo(currency) {
   useEffect(() => {
     fetch(
       //This url has the currency json of a currency to all other currency (latest)
-      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+      // `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
     )
       .then((res) => res.json())
       .then((res) => setData(res[currency]));
